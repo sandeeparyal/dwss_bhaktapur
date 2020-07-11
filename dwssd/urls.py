497021959abef9429976ec71f2520db4867b735d
Sandeep Aryal <sandeeparyal@gmail.com>
2020-07-11 17:59:23 +0545

    Debug=False try

--- dwssd/urls.py
+++ dwssd/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-        path('wsasd5/', include('wsasd5.urls')),
+        path('', include('wsasd5.urls')),
         path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
